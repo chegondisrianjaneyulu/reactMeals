3ebb22c5134cb01fd2479d64cfe07b0bb8243f27
Sri <chegondisrianjaneyulu@gmail.com>
2023-01-27 17:55:25 +0530

    ui-changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import Mainpage from './components/mainpage/mainpage';
-import HtmlData from './components/htmlData/htmlData';
 import Button from './components/button/button';
-import Transforms from './components/transforms/transforms';
+
 
 
 function App() {
   return (
     <>
      <Mainpage/>
-    {/* <HtmlData/> */}
     <Button/>
-     {/* <Transforms/> */}
+     
   </>
   );
 }
